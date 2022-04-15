40275c7d44faa64ed71c3cfbc8e443b318f0e65a
shwjn515 <70755688+shwjn515@users.noreply.github.com>
2022-04-15 11:25:39 +0800

    Initial commit

---
+++ src/main/java/com/test/ayard/AyardApplication.java
@@ -0,0 +1,13 @@
+package com.test.ayard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyardApplication.class, args);
+    }
+
+}
